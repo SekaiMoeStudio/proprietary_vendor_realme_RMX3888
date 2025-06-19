f4746bfddb2dc9faae85700868b84bf22586db0a
dabao1955 <dabao1955@163.com>
2025-06-19 12:13:00 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,3 +5,7 @@ set -e
 tar -xvf $(pwd)/libarcsoft_turbo_hdr_raw.so.tar.xz
 
 mv libarcsoft_turbo_hdr_raw.so proprietary/odm/lib64/libarcsoft_turbo_hdr_raw.so
+
+wget https://github.com/SekaiMoeStudio/proprietary_vendor_realme_RMX3888/releases/download/15.0.0.232v2/radio.tar.xz
+
+tar -xvf radio.tar.xz
